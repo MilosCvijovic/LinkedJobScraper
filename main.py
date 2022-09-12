2e1001bfa72fd0526b212e744ed4643c9fb63cd5
Misa <miloscvijovic109@gmail.com>
2022-09-12 11:02:25 +0200

    Project done

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import requests
 from bs4 import BeautifulSoup
-import csv
 from selenium import webdriver
 import time
-from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import pandas as pd
+import re
+
 
 # Open tab in incognito mode in order to avoid previous data being loaded, such as
 # cookies, site data or information entered in forms saved on the device.
@@ -16,7 +16,7 @@ options.add_argument("--incognito")
 # Setting experimental option 'detach' to True in order to window stay open.
 options.add_experimental_option('detach', True)
 
-# Get the webdriver path for Linkedin Homepage.
+# Get the web driver path for Linkedin Homepage.
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 driver.maximize_window()
 url = 'https://www.linkedin.com/home'
@@ -39,7 +39,7 @@ password.send_keys(passcode)
 
 time.sleep(2)
 
-# Login in.
+# Click login in button.
 driver.find_element(By.XPATH, "//button[@type = 'submit']").click()
 time.sleep(2)
 
@@ -47,9 +47,59 @@ time.sleep(2)
 driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3247607301&geoId=101855366&keywords=python%20internship&location=Serbia&refresh=true")
 time.sleep(3)
 
-# Scroll down to load all available offers
+# Try to load pages if there are moore then one
+try:
+    for i in range(1, 3):
+        # click button to change the job list
+        driver.find_element(By.XPATH, f'//button[@aria-label="Page {i}"]').click()
+        time.sleep(2)
+except:
+    pass
+
+# Scroll down all job offers to collect necessary information.
 element = driver.find_element(By.XPATH, '//*[@id="main"]/div/section[1]/div')
 for i in range(20):
     driver.execute_script("arguments[0].scrollBy(0, 500)", element)
     time.sleep(2)
 
+# Collecting data to extract(job titles, location, company name).
+job_src = driver.page_source
+soup = BeautifulSoup(job_src, 'lxml')
+
+jobs_html = soup.find_all('a', {'class': 'job-card-list__title'})
+
+job_titles = []
+
+for title in jobs_html:
+    job_titles.append(title.text.strip())
+
+print(job_titles)
+
+location_html = soup.find_all(
+    'ul', {'class': 'job-card-container__metadata-wrapper'})
+
+location_list = []
+
+for loc in location_html:
+    res = re.sub('\n\n +', ' ', loc.text.strip())
+
+    location_list.append(res)
+
+print(location_list)
+
+
+company_name_html = soup.find_all(
+    'a', {'class': 'job-card-container__company-name'})
+company_names = []
+
+for name in company_name_html:
+    company_names.append(name.text.strip())
+
+print(company_names)
+
+
+# Creating a Dataframe with list
+df = pd.DataFrame(list(zip(job_titles, location_list, company_names)))
+
+# exporting our dataframe to a csv file
+df.to_csv('jobs.csv', sep=';')
